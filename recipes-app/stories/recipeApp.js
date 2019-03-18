f09cc3bd3aeb1549c0cd28b9c554e5ed575c0ce7
Padraig O Neill <contact@padraigoneill.net>
2019-03-18 23:33:22 +0000

    Updated storybooks

--- recipes-app/stories/recipeApp.js
+++ recipes-app/stories/recipeApp.js
@@ -3,86 +3,65 @@ import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import SelectControls from '../src/components/selectControls';
 import Recipe from '../src/components/recipe';
-import Recipes from '../src/Data';   // Test data set
+// import Recipes from '../src/Data';   // Test data set
 import RecipeList from '../src/components/recipeList';
 import { action } from '@storybook/addon-actions';
 import Images from '../src/components/recipeDetail/images';
 import { MemoryRouter, Route } from 'react-router'
 import Details from '../src/components/recipeDetail/details'
+import api from '../src/dataStore/stubAPI';
 
 
 const recipe = 
-        {
-            "additionalFeatures": "Front Facing 1.3MP Camera", 
-            "android": {
-                    "os": "Android 2.2", 
-                    "ui": "Dell Stage"
-            }, 
-            "availability": [
-                    "T-Mobile"
-            ], 
-            "battery": {
-                    "standbyTime": "", 
-                    "talkTime": "", 
-                    "type": "Lithium Ion (Li-Ion) (2780 mAH)"
-            }, 
-            "camera": {
-                    "features": [
-                            "Flash", 
-                            "Video"
-                    ], 
-                    "primary": "5.0 megapixels"
-            }, 
-            "connectivity": {
-                    "bluetooth": "Bluetooth 2.1", 
-                    "cell": "T-mobile HSPA+ @ 2100/1900/AWS/850 MHz", 
-                    "gps": true, 
-                    "infrared": false, 
-                    "wifi": "802.11 b/g"
-            }, 
-            "description": "Introducing Dell\u2122 Streak 7. Share photos, videos and movies together. It\u2019s small enough to carry around, big enough to gather around. Android\u2122 2.2-based tablet with over-the-air upgrade capability for future OS releases.  A vibrant 7-inch, multitouch display with full Adobe\u00ae Flash 10.1 pre-installed.  Includes a 1.3 MP front-facing camera for face-to-face chats on popular services such as Qik or Skype.  16 GB of internal storage, plus Wi-Fi, Bluetooth and built-in GPS keeps you in touch with the world around you.  Connect on your terms. Save with 2-year contract or flexibility with prepaid pay-as-you-go plans", 
-            "display": {
-                    "screenResolution": "WVGA (800 x 480)", 
-                    "screenSize": "7.0 inches", 
-                    "touchScreen": true
-            }, 
-            "hardware": {
-                    "accelerometer": true, 
-                    "audioJack": "3.5mm", 
-                    "cpu": "nVidia Tegra T20", 
-                    "fmRadio": false, 
-                    "physicalKeyboard": false, 
-                    "usb": "USB 2.0"
-            }, 
-            "id": "dell-streak-7", 
-            "images": [
-                    "img/recipes/dell-streak-7.0.jpg", 
-                    "img/recipes/dell-streak-7.1.jpg", 
-                    "img/recipes/dell-streak-7.2.jpg", 
-                    "img/recipes/dell-streak-7.3.jpg", 
-                    "img/recipes/dell-streak-7.4.jpg"
-            ], 
-            "name": "Dell Streak 7", 
-            "sizeAndWeight": {
-                    "dimensions": [
-                            "199.9 mm (w)", 
-                            "119.8 mm (h)", 
-                            "12.4 mm (d)"
-                    ], 
-                    "weight": "450.0 grams"
-            }, 
-            "storage": {
-                    "flash": "16000MB", 
-                    "ram": "512MB"
-            }
-        }
+{
+        "ingredients":  [
+            "6 skinless, boneless chicken breast halves - cut into 1/2 inch strips",
+            "1 egg, beaten",
+            "1 cup buttermilk",
+            "1 1/2 teaspoons garlic powder",
+            "1 cup all-purpose flour",
+            "1 cup seasoned bread crumbs",
+            "1 teaspoon salt",
+            "1 teaspoon baking powder",
+            "1 quart oil for frying"
+            
+        ], 
+        "time": {
+            "prepTime": "20m", 
+            "cookTime": "10m", 
+            "readyTime": "2h 30m"
+        }, 
+        
+        "directions": [
+            "Place chicken strips into a large, resealable plastic bag. In a small bowl, mix the egg, buttermilk and garlic powder. Pour mixture into bag with chicken. Seal, and refrigerate 2 to 4 hours.",
+            "In another large, resealable plastic bag, mix together the flour, bread crumbs, salt and baking powder. Remove chicken from refrigerator, and drain, discarding buttermilk mixture. Place chicken in flour mixture bag. Seal, and shake to coat.",
+            "Heat oil in a large, heavy skillet to 375 degrees F (190 degrees C).",
+            "Carefully place coated chicken in hot oil. Fry until golden brown and juices run clear. Drain on paper towels."
+        ],
+        "upvotes": 5, 
+        
+        "description": "If you like the taste of garlic, this recipe is for you. It is easy to make, but requires time to marinate.", 
+    
+        "id": "breaded-chicken-fingers", 
+        "images": [
+            "img/recipes/breaded-chicken-fingers.0.jpg", 
+            "img/recipes/breaded-chicken-fingers.1.jpg", 
+            "img/recipes/breaded-chicken-fingers.2.jpg",
+            "img/recipes/breaded-chicken-fingers.4.jpg", 
+            "img/recipes/breaded-chicken-fingers.5.jpg"
+        ], 
+        "name": "Breaded Chicken Fingers"
+    }
+    
+
+const recipes = api.getAll();
 
 storiesOf('Recipe List', module)
         .add('Images', 
-        () =>  <Images recipe={recipe} />
+        () =>  <Images recipe={recipe}  />
         ) 
         .add('Details', 
-        () =>  <Details recipe={recipe} />
+        () =>  <Details recipe={recipe}  />
         )
         .addDecorator(story => 
                 <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
@@ -94,12 +73,11 @@ storiesOf('Recipe List', module)
                     filterText='' /> 
         )
         .add('Recipe', 
-        () =>  <Recipe recipe={Recipes[0]} /> 
+        () =>  <Recipe recipe={recipes[0]} /> 
         )
         .add('Recipe list', 
-        () =>  <RecipeList recipes={Recipes} /> 
+        () =>  <RecipeList recipes={recipes} /> 
         )
-        // .add('Images', 
-        // () =>  <Images recipe={recipe} />
-        // )      
+        
+
  
